26be9e00107fb8e87da557e6aada31e2fee1f53a
AFellowCoderUser <gotimemel@outlook.com>
2023-11-09 23:08:16 -0500

    im going crazy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function (eleventyConfig) {
+
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/images");
     eleventyConfig.addPassthroughCopy("./src/fonts");
     eleventyConfig.addPassthroughCopy("./src/js");
+
+    eleventyConfig.addFilter("limit", function (array, limit) {
+      return array.slice(0, limit);
+    });
 
     return {
     passthroughFileCopy: true,

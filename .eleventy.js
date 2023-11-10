830c543298bc5a79079055187aada117ef325456
AFellowCoderUser <109687165+AFellowCoderUser@users.noreply.github.com>
2023-11-10 17:23:05 -0500

    Changed HTML to Nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/fonts");
     eleventyConfig.addPassthroughCopy("./src/js");
 
+    eleventyConfig.addWatchTarget("./src/_includes/layout");
+
     eleventyConfig.addFilter("limit", function (array, limit) {
       return array.slice(0, limit);
     });
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mebi-neo-11ty
+
+11ty'd version of my website on [Neocities](https://afellowu.neocities.org/).
+
+This is where I'm working on a new site layout for my website.
+
+You may have noticed there is a separate repo named "mebi-neo-11ty-pract", well I forgot that repo branches existed :P

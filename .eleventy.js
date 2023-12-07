3d133a72076c47d2c3d14c2ce5f87b8d2d369a40
AFellowCoderUser <gotimemel@outlook.com>
2023-12-06 22:22:25 -0500

    Configured .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,82 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
+const sass = require("sass");
+const path = require("node:path");
+const browserlist = require("browserslist");
+const { bundle, transform, browserslistToTargets, composeVisitors } = require("lightningcss");
 
 module.exports = function (eleventyConfig) {
-  
-    eleventyConfig.addPassthroughCopy("./src/css");
+
+    //De-index.html the output
+    eleventyConfig.addGlobalData("permalink", () => {
+      return (data) => `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+    });
+
+    //Add Sass support
+    eleventyConfig.addTemplateFormats("scss");
+    eleventyConfig.addExtension("scss", {
+      outputFileExtension: "css",
+
+      compile: async function(inputContent, inputPath) {
+        let parsed = path.parse(inputPath);
+        if (parsed.name.startsWith("_")) {
+          return;
+        }
+
+        let result = sass.compileString(inputContent, {
+          loadPaths: [parsed.dir || "."],
+          sourceMap: false,
+        });
+
+        this.addDependencies(inputPath, result.loadedUrls);
+
+        let targets = browserslistToTargets(browserlist("> 0.2% and not dead"))
+
+        return async () => {
+          let { code } = await transform({
+            code: Buffer.from(result.css),
+            minify: true,
+            sourceMap: false,
+            targets,
+          });
+          return code;
+        };
+      },
+    });
+
+    //Minify CSS if it ends with "min.css"
+    eleventyConfig.addTemplateFormats("min.css");
+    eleventyConfig.addExtension("min.css", {
+      outputFileExtension: "min.css",
+
+      compile: async function(_inputContent, inputPath) {
+        
+        let targets = browserslistToTargets(browserlist("> 0.2% and not dead"))
+
+        return async () => {
+          let { code } = await bundle({
+            filename: inputPath,
+            minify: true,
+            sourceMap: false,
+            targets,
+          });
+          return code;
+        };
+      },
+    });
+
+    //Get assets
     eleventyConfig.addPassthroughCopy("./src/images");
     eleventyConfig.addPassthroughCopy("./src/fonts");
     eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.setTemplateFormats([
+      "njk",
+      "md",
+      "css"
+    ]);
 
     eleventyConfig.addWatchTarget("./src/_includes/layout");
 
+    //Filters
     eleventyConfig.addFilter("limit", function (array, limit) {
       return array.slice(0, limit);
     });
@@ -20,8 +87,8 @@ module.exports = function (eleventyConfig) {
       });
     });
 
+    //Plugins
     eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(bundlerPlugin);
 
     return {
     passthroughFileCopy: true,

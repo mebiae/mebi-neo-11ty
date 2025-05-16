72ed9087b6605f013ba7174bfd6902c35ad78b29
mebiae <109687165+mebiae@users.noreply.github.com>
2025-05-15 21:10:29 -0400

    replace js dropdowns with details elements

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,21 +1,21 @@
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+toggle between hiding and showing the dropdown content
 function drop() {
     for (i = 0; i < document.getElementsByClassName("drop-content").length; i++) {
       document.getElementsByClassName("drop-content")[i].style.display = "block";
     }
 }
-  
+
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.drop-button')) {
-      var dropdowns = document.getElementsByClassName("drop-content");
+    if (event.target.matches('.drop')) {
+      var dropdowns = document.getElementsByClassName("drop");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
-        if (openDropdown.style.display == "block") {
-          openDropdown.style.display = "";
+        if (openDropdown.hasAttribute("open")) {
+          openDropdown.toggleAttribute("open");
         }
       }
     }
-  }
+  }*/
